3dff0738cb851d4083f2ab986849dc65eea31b28
Baltenko Mark <markbaltenko@gmail.com>
2024-05-11 00:55:07 +0300

    [test]

--- src/modules/posts/posts.service.ts
+++ src/modules/posts/posts.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { Posts } from '@prisma/client';
+import { PrismaClient, Prisma } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 @Injectable()
 export class PostsService {
-  findAll(): string[] {
-    return ['dragons', 'kittens', 'unicorns'];
+
+  findAll(): Array<string> {
+    return ['1', '2', '3'];
   }
+
+
+  // async createNewPost(data: any): Promise<Posts> {
+  //   return await prisma.posts.create({
+  //     data: {
+  //       title: data.title,
+  //       description: data.description,
+  //       imgUrl: data.image,
+  //     },
+  //   });
+  // }
 }
