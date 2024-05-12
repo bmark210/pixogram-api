c602ca5d01f0d074758e24857c3a9bde8fc0f948
Baltenko Mark <markbaltenko@gmail.com>
2024-05-12 21:00:18 +0300

    [fix]

--- src/modules/posts/posts.controller.ts
+++ src/modules/posts/posts.controller.ts
@@ -27,7 +27,7 @@ export class PostsController {
     return this.postsService.updatePost(id, body);
   }
 
-  @Post(":id/delete")
+  @Delete(":id/delete")
   deletePost(@Query() id: number) {
     return this.postsService.deletePost(id);
   }
