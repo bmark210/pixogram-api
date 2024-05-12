34ae4eb40843275a8dfd2e36438a7b090f97bbd8
Baltenko Mark <markbaltenko@gmail.com>
2024-05-12 20:45:01 +0300

    [add] posts interfaces for update

---
+++ src/interfaces/posts.interface.ts
@@ -0,0 +1,18 @@
+export interface ICreatePostDto {
+    title: string;
+    description: string;
+    imgUrl: string;
+}
+export interface IUpdatePostDto {
+    title?: string;
+    description?: string;
+    imgUrl?: string;
+}
+
+export interface IPostsFilter {
+    searchText?: string;
+    limit?: number;
+    offset?: number;
+    sortBy?: string;
+    sortOrder?: string;
+}
