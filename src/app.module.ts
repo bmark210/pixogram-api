4a0985243cc10872bee4d66b5621d9403b3439ec
Baltenko Mark <markbaltenko@gmail.com>
2024-05-12 14:25:03 +0300

    [fix]

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsModule } from './modules/posts/posts.module';
 import { UserModule } from './modules/user/user.module';
-import { AuthModule } from './modules/prisma/prisma.module';
 
 @Module({
-  imports: [UserModule, PostsModule, AuthModule],
+  imports: [UserModule, PostsModule],
   controllers: [AppController],
   providers: [AppService],
 })
