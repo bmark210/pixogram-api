9e99333e1ac24ce82c290778bb6dad56290a2298
Baltenko Mark <markbaltenko@gmail.com>
2024-07-29 17:24:01 +0300

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,12 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
-  // app.enableCors({
-  //   origin: true,
-  //   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-  //   credentials: true,
-  // });
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: true,
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
