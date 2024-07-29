7643d97fd112a5e9f261ac9e09592b8d519b5a0b
Baltenko Mark <markbaltenko@gmail.com>
2024-07-29 17:40:51 +0300

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['https://localhost:4200'],
-    preflightContinue: true,
+    origin: true,
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
     credentials: true,
   });
