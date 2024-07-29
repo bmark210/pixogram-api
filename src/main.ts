9636de93544025f043dd6237b79ae0113dd2e8a1
Baltenko Mark <markbaltenko@gmail.com>
2024-07-29 17:35:59 +0300

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: ['https://localhost:4200'],
+    preflightContinue: true,
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
     credentials: true,
   });
