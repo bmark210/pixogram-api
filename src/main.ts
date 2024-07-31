f47fb5fc88605e72aa80926db7620fe39887c791
Baltenko Mark <markbaltenko@gmail.com>
2024-07-31 13:18:12 +0300

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
     origin: true,
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
     credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization'],
   });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
